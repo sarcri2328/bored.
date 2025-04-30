306468695370a684954c62cb0110dd2bdf70104e
Sarah Criswell <sarcri2328@students.ecpi.edu>
2025-04-29 20:58:33 -0400

    Added suggestion interface. Needs more work

---
+++ src/Suggestion.java
@@ -0,0 +1,14 @@
+/* 
+ * Name: Sarah Criswell
+ * Date: 4/29/2025
+ * Assignment: SDC330 Week 3 Course Project
+ * 
+ * Class Description: This is the Suggestion interface where both indoor and outdoor
+ * Activity suggestions will be generated from.
+ */
+
+public interface Suggestion {
+
+    //More to be added in this section
+    
+} 
