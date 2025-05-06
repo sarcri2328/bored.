63cc7e99f17fe164dccd5b0f57301525ab8a2e86
Sarah Criswell <sarcri2328@students.ecpi.edu>
2025-05-06 14:22:41 -0400

    Added constructors to Activity class

--- src/Activity.java
+++ src/Activity.java
@@ -4,13 +4,38 @@
  * Assignment: SDC330 Week 3 Course Project
  * 
  * Class Description: The Activity class is the super class of IndoorActivity and OutdoorActivity.
- * It is an abstract class.
+ * It contains getters for each field in the database, and constructors for the Database.
  */
 
-public abstract class Activity {
-    
-    //Empty methods
-    public abstract String getDescription();
-    public abstract String getLocation();
-    public abstract String getActivity();
+public class Activity {
+    public int ID;
+    public String Activity;
+    public String Location;
+    public String Description;
+
+    public Activity(int iD, String activity, String location, String description) {
+        ID = iD;
+        Activity = activity;
+        Location = location;
+        Description = description;
+    }
+
+    public Activity(String activity, String location, String description) {
+        Activity = activity;
+        Location = location;
+        Description = description;
+    }
+
+    public Activity() {
+    }
+
+    public String getDescription(){
+        return Description;
+    }
+    public String getLocation(){
+        return Location;
+    }
+    public String getActivity(){
+        return Activity;
+    }
 }
