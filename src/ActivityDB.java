96b03549ba8bfd63fa3af53ffe7d6ef4b4a16847
Sarah Criswell <sarcri2328@students.ecpi.edu>
2025-05-11 15:27:44 -0400

    Updated header, moved array lists to ActivityList class, delete unneccesarry methods that will not be used.

--- src/ActivityDB.java
+++ src/ActivityDB.java
@@ -1,7 +1,7 @@
 /*******************************************************************
 * Name: Sarah Criswell
-* Date: 5/6/2025
-* Assignment: SDC330 Week 3 Course Project
+* Date: 5/11/2025
+* Assignment: SDC330 Course Project - bored. 
 *
 * Class to handle all interactions with the Activities table in the
 * database, including creating the table if it doesn't exist and all
@@ -14,10 +14,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ActivityDB {
+
+    //Method for creating a new table at the beginning of the application if one does not already exist
     public static boolean createTable(Connection conn) {
         // SQL statement for creating a new table
         String sql =
@@ -45,6 +46,7 @@ public class ActivityDB {
         }
     }
 
+    //Method for adding a new activity. This is only used for manually adding template data at beginning of the app creation. It will not be used again or able to be used by user in the terminal.
     public static void addActivity(Connection conn, Activity a){
         String sql =
             "INSERT INTO Activities(Activity, Location, Description) VALUES(?,?,?)";
@@ -68,6 +70,7 @@ public class ActivityDB {
         }
     }
 
+    //Method for adding new activities in the terminal. Contains a scanner for user input, and adds the activity to the database.
     public static void addActivityPrompt(Connection conn){
         Scanner scanner = new Scanner(System.in);
         Activity newActivity = new Activity();
@@ -104,29 +107,7 @@ public class ActivityDB {
         }
     }
 
-    public static void updateActivity(Connection conn, Activity a) {
-        String sql =
-            "UPDATE Activities SET Activity=?, Location=?, Description=? WHERE id=?";
-        PreparedStatement pst = null;
-        try {
-            pst = conn.prepareStatement(sql);
-            pst.setString(1, a.Activity);
-            pst.setString(2, a.Location);
-            pst.setString(3, a.Description);
-            pst.executeUpdate();
-        } catch(SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            if (pst != null){
-                try {
-                    pst.close();
-                } catch (SQLException e) {
-                    System.out.println("Error closing prepared statement: " + e.getMessage());
-                }
-            }
-        }
-    }
-
+    //Method for updating activities via the terminal. It contains a scanner that is used to take in user input and update the database.
     public static void updateActivityPrompt(Connection conn){
         Scanner updateScanner = new Scanner(System.in);
         System.out.println("Enter the ID of the activity you want to update: ");
@@ -180,6 +161,7 @@ public class ActivityDB {
         }
     }
 
+    //Method for deleting activities via the terminal. It contains a scanner that is used to take in user input and delete specific rows from the database.
     public static void deleteActivityPrompt(Connection conn) {
 
         Scanner deleteScanner = new Scanner(System.in);
@@ -209,86 +191,7 @@ public class ActivityDB {
         }
     }
 
-    public static void deleteActivity(Connection conn, int id) {
-        String sql = "DELETE from Activities WHERE ID=?";
-        PreparedStatement pst = null;
-        try {
-            pst = conn.prepareStatement(sql);
-            pst.setInt(1, id);
-            pst.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public static ArrayList<Activity> getAllIndoorActivities(Connection conn) {
-        ArrayList<Activity> indoorActivities = new ArrayList<Activity>();
-        String sql = "SELECT * FROM Activities WHERE Location='Indoors'";
-    
-        Statement stmt = null;
-        ResultSet rs = null;
-        try {
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery(sql);
-            while (rs.next()) {
-                Activity a = new Activity(rs.getInt("ID"), rs.getString("Activity"),
-                    rs.getString("Location"), rs.getString("Description"));
-                    indoorActivities.add(a);
-            }   
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                    System.out.println("Error closing result set: " + e.getMessage());
-                }
-            } if (stmt != null) {
-                try {
-                    stmt.close();
-                } catch (SQLException e) {
-                    System.out.println("Error closing statement: " + e.getMessage());
-                }
-            }
-        }
-        return indoorActivities;
-    }
-
-    public static ArrayList<Activity> getAllOutdoorActivities(Connection conn) {
-        ArrayList<Activity> outdoorActivities = new ArrayList<Activity>();
-        String sql = "SELECT * FROM Activities WHERE Location='Outdoors'";
-    
-        Statement stmt = null;
-        ResultSet rs = null;
-        try {
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery(sql);
-            while (rs.next()) {
-                Activity a = new Activity(rs.getInt("ID"), rs.getString("Activity"),
-                    rs.getString("Location"), rs.getString("Description"));
-                    outdoorActivities.add(a);
-            }   
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                    System.out.println("Error closing result set: " + e.getMessage());
-                }
-            } if (stmt != null) {
-                try {
-                    stmt.close();
-                } catch (SQLException e) {
-                    System.out.println("Error closing statement: " + e.getMessage());
-                }
-            }
-        }
-        return outdoorActivities;
-    }
-
+    //Method for finding activities using SQL select statements. This is used in the update and delete methods above.
     public static Activity getActivity(Connection conn, int id) {
         Activity a = new Activity();
         String sql = "SELECT * FROM Activities WHERE ID=?";
